8241e6174c72d294039fd9976f18b7eebca775dd
about-joo91 <about.joo91@gmail.com>
2022-06-04 17:24:32 +0900

    func/회원가입 기능 초기 구현

--- user/views.py
+++ user/views.py
@@ -1,7 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import auth
+from .models import UserModel
 
 # Create your views here.
 def sign_up(request):
+    if request.method == 'POST':
+        username = request.POST.get('new_username',None)
+        password = request.POST.get('new_password',None)
+        nickname = request.POST.get('nickname',None)
+        new_user = UserModel.objects.create_user(
+            username = username,
+            password = password,
+            nickname = nickname
+        )
+        new_user.save()
+        return redirect('/user/sign_in/')
     return render(request, 'user/sign_up.html')
 
 def sign_in(request):
