a67414667963dd0c1303a80508141034d3271a43
about-joo91 <about.joo91@gmail.com>
2022-06-04 15:31:31 +0900

    feature/사인인 페이지 뼈대 구성

--- user/urls.py
+++ user/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('', views.sign_up, name='sign_up')
+    path('sign_up/', views.sign_up, name='sign_up'),
+    path('sign_in/', views.sign_in, name='sign_in')
 ]
