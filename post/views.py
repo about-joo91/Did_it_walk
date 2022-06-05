fb07de6698ad8c0aa19fe4c455bdb9d03bdf7b0f
khw7876 <skanahrwi@naver.com>
2022-06-05 17:46:14 +0900

    feature/main 메인페이지 image upload 구현

--- post/views.py
+++ post/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from .forms import *
 
 # Create your views here.
 def home(request):
@@ -7,7 +8,15 @@ def home(request):
     #     return redirect('main')
     # else:
     #     return redirect('/user/sign_in')
-    return redirect('main')
+    return redirect('/main')
 
 def main(request):
-    return render(request, 'post/main.html')
+    if request.method == 'POST':
+        print("post방식이다.")
+        form = UploadForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+        return redirect('/post/home')
+    elif request.method == 'GET':
+        form = UploadForm()
+    return render(request, 'post/main.html',{'form':form})
