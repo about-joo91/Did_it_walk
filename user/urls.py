2544dd669bb2df487580b026438ae5095d47ad00
about-joo91 <about.joo91@gmail.com>
2022-06-05 23:02:10 +0900

    feature/팔로우 페이지 작업완료 #17

--- user/urls.py
+++ user/urls.py
@@ -3,5 +3,6 @@ from . import views
 urlpatterns = [
     path('sign_up/', views.sign_up, name='sign_up'),
     path('sign_in/', views.sign_in, name='sign_in'),
-    path('sign_out/', views.sign_out, name='sign_out')
+    path('sign_out/', views.sign_out, name='sign_out'),
+    path('follower/', views.follow_page, name='follow_page'),
 ]
