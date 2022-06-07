2358d3e9fc3d34d421a773c52a1ecd9fb81656bd
khw7876 <skanahrwi@naver.com>
2022-06-07 21:22:48 +0900

    feature/main shoe_tag 구현, 에러메세지 구현

--- post/admin.py
+++ post/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Post_Img, Posts
+from .models import Post_Img, Post, Shoe_tag
 # Register your models here.
 admin.site.register(Post_Img)
-admin.site.register(Posts)
+admin.site.register(Post)
+admin.site.register(Shoe_tag)
--- post/models.py
+++ post/models.py
@@ -4,14 +4,23 @@ from django.db import models
 from user.models import UserModel
 
 # Create your models here.
-class Posts(models.Model):
+class Post(models.Model):
     class Meta:
         db_table = "Posts"
     contents = models.TextField()
     users = models.ForeignKey(UserModel, on_delete=models.CASCADE)
 
+
 class Post_Img(models.Model):
     class Meta:
         db_table = "Post_Img"
-    posts = models.ForeignKey('Posts', on_delete=models.CASCADE)
-    post_img_url = models.ImageField(upload_to="media/")
+    posts = models.ForeignKey('Post', on_delete=models.CASCADE)
+    post_img = models.ImageField(upload_to="media/")
+
+class Shoe_tag(models.Model):
+    class Meta:
+        db_table = "Shoe_tags"
+    tag_title = models.CharField(max_length=50)
+    tag_price = models.IntegerField()
+    tag_image_url = models.URLField("Site_URL")
+    post_tags = models.ManyToManyField('Post')
