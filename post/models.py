e7cc3ce9ac38e413a041e1b0dfb0db6c7a0884c1
khw7876 <skanahrwi@naver.com>
2022-06-09 01:01:50 +0900

    feature/main 서버측에서 데이터 받아오기 구현

--- post/models.py
+++ post/models.py
@@ -4,19 +4,6 @@ from django.db import models
 from user.models import UserModel
 
 # Create your models here.
-class Post(models.Model):
-    class Meta:
-        db_table = "Post"
-    contents = models.TextField()
-    users = models.ForeignKey(UserModel, on_delete=models.CASCADE)
-    shoe_tags = models.ManyToManyField('Shoe_tag', related_name='posts')
-
-class Post_Img(models.Model):
-    class Meta:
-        db_table = "Post_Img"
-    post = models.ForeignKey('Post', on_delete=models.CASCADE)
-    post_img = models.ImageField(upload_to="media/")
-
 class Shoe_tag(models.Model):
     class Meta:
         db_table = "Shoe_tag"
@@ -24,3 +11,18 @@ class Shoe_tag(models.Model):
     tag_price = models.IntegerField()
     tag_image_url = models.URLField("Site_URL")
     
+
+class Post(models.Model):
+    class Meta:
+        db_table = "Post"
+    contents = models.TextField()
+    user = models.ForeignKey(UserModel, on_delete=models.CASCADE)
+    shoe_tags = models.ManyToManyField(Shoe_tag, related_name='shoe_taggie')
+
+
+class Post_Img(models.Model):
+    class Meta:
+        db_table = "Post_Img"
+    post = models.ForeignKey('Post', on_delete=models.CASCADE)
+    post_img = models.ImageField(upload_to="media/")
+
