9f6b9708d58132389e4ed989f1db92566b3c1b83
khw7876 <skanahrwi@naver.com>
2022-06-12 02:38:08 +0900

    refac/main post의models 수정

--- post/models.py
+++ post/models.py
@@ -4,7 +4,7 @@ from django.db import models
 from user.models import UserModel
 
 # Create your models here.
-class Shoe_tag(models.Model):
+class ShoeTag(models.Model):
     class Meta:
         db_table = "Shoe_tag"
     tag_title = models.CharField(max_length=50)
@@ -17,12 +17,12 @@ class Post(models.Model):
         db_table = "Post"
     contents = models.TextField()
     user = models.ForeignKey(UserModel, on_delete=models.CASCADE)
-    shoe_tags = models.ManyToManyField(Shoe_tag, related_name='shoe_taggie')
+    shoe_tags = models.ManyToManyField(ShoeTag, related_name='shoe_taggie')
+    post_img = models.ForeignKey('PostImg', on_delete=models.CASCADE)
 
-
-class Post_Img(models.Model):
+class PostImg(models.Model):
     class Meta:
         db_table = "Post_Img"
-    post = models.ForeignKey('Post', on_delete=models.CASCADE)
+    # post = models.ForeignKey('Post', on_delete=models.CASCADE)
     post_img = models.ImageField(upload_to="media/")
 
