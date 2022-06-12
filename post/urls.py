f3cc7584ac8f832d042106cc9daa4e8ed06150a3
about-joo91 <about.joo91@gmail.com>
2022-06-13 04:25:22 +0900

    enhance/ 디테일 댓글기능 추가

--- post/urls.py
+++ post/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path('home/<str:page_name>', views.main, name = 'recent2'),
     path('like/<int:post_id>', views.like, name ='like_post'),
     path('comment/<int:post_id>', views.comment, name ='comment'),
+    path('comment/<int:post_id>/<int:comment_id>', views.comment, name='comment'),
+    path('comment/edit/<int:comment_id>', views.comment_edit, name='comment_edit'),
 ]
